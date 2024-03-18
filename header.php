c5b2a4f19b366c88505e86c764f3aee3fff8834e
Joly Sonia <sonia.joly38@gmail.com>
2024-03-18 17:04:45 +0100

    integration of the logo in the header

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=<?php language_attributes(); ?>> <!-- Définit automatiquement la langue -->
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>"> <!-- Permet de définir l'encodage du site -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>> <!-- Permet d’obtenir des noms de classe CSS en fonction de la page visitée -->
+
+    <?php wp_body_open(); ?>
+        <header class="header">
+            <div>
+                <img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-2.png" alt="Logo" alt="">
+            </div>
+        </header>
+    
+</body>
+</html>
