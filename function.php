c29c660adcca30f9567ee3a8854a5362f9dea8e2
Joly Sonia <sonia.joly38@gmail.com>
2024-03-20 09:42:31 +0100

    modification function menu nav

--- function.php
+++ function.php
@@ -1,14 +1,16 @@
 <?php
 // Style personnalisé du thème 
 function nathalie_mota_theme_enqueue(){
-    wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri(), '/style.css', array(), '1.0');
+    wp_enqueue_syle('nathalie-mota', get_stylesheet_uri());
+    wp_enqueue_style('theme-style', get_template_directory_uri() . '/assets/css/style.css');
     filemtime(get_stylesheet_directory() . '/css/theme.css');
 }
 add_action('wp_enqueue_scripts','nathalie-mota-theme_enqueue');
 // Gestion du menu 
-function my_menu_register(){
-    register_nav_menu('main_menu',__('Menu principal'));
-    register_nav_menu('footer_menu',__('Menu footer'));
+// Enregistrement - Menu Principal
+function register_my_menu() {
+    register_nav_menu( 'main-menu', __( 'Menu principal', 'text-domain' ) );
 }
-add_action('init','my_menu_register');
+add_action( 'after_setup_theme', 'register_my_menu' );
+
 ?>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"> <!-- Permet de définir l'encodage du site -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>> <!-- Permet d’obtenir des noms de classe CSS en fonction de la page visitée -->
@@ -10,18 +11,18 @@
     <?php wp_body_open(); ?>
         <header id="header">
             <div class="logo">
-                <img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-2.png" alt="Logo">
-            </div>
-            <nav class="main-menu">
+                <a href="http://nathaliemota.local"><img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-2.png" alt="Logo"></a>
+                <nav class="main-menu">
                 <?php
-                 // Affichage du menu main déclaré dans function.php
-                 wp_nav_menu([
-                    'theme_location' => 'main_menu',
-                    'container'=>'false',
-                    'menu_class'=>'main-menu'
-                ]);
-                 ?>
-            </nav>
+                 // Affiche | Menu de navigation en utilisant un emplacement de thème nommé 'main-menu'
+                    wp_nav_menu([
+                        'theme_location' => 'main-menu',
+                        'container'      => false
+                    ]);
+                ?>
+                </nav>
+            </div>
+            
         </header>
     
 </body>
