48b49ac5cd74e7c76a09e263b877a0e2b6c1ccf6
Joly Sonia <sonia.joly38@gmail.com>
2024-03-21 16:05:03 +0100

    declaration fichier script

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action('wp_enqueue_scripts','nathalie_mota_theme_enqueue');
 
 // Script personnalisé du thème 
 function nathalie_mota_scripts(){
-    wp_enqueue_script('nathalie-scripts', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('nathalie-scripts', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts','nathalie_mota_scripts');
 // Gestion du menu 
