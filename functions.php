cf866e1d3fb50716b6ade8962794f1a9183c1545
Joly Sonia <sonia.joly38@gmail.com>
2024-03-21 08:41:55 +0100

    correction of theme function name

--- functions.php
+++ functions.php
@@ -1,17 +1,11 @@
 <?php
-// Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
-
-// Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support( 'title-tag' );
 
 // Style personnalisé du thème 
 function nathalie_mota_theme_enqueue(){
-    wp_enqueue_syle('nathalie-mota', get_stylesheet_uri());
+    wp_enqueue_style('nathalie-mota', get_stylesheet_uri());
     wp_enqueue_style('theme-style', get_template_directory_uri() . '/assets/css/style.css');
-    filemtime(get_stylesheet_directory() . '/css/theme.css');
 }
-add_action('wp_enqueue_scripts','nathalie-mota-theme_enqueue');
+add_action('wp_enqueue_scripts','nathalie_mota_theme_enqueue');
 // Gestion du menu 
 
 // créer un lien pour la gestion des menus dans l'administration et activation d'un menu pour le header et d'un menu pour le footer.
