004884b77e00ca8a9697975b9f22d010788432e9
Joly Sonia <sonia.joly38@gmail.com>
2024-03-27 10:46:45 +0100

    Creation templates_post

---
+++ templates/template_post.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Template Post
+ */
+
+ get_hheader();
+ ?>
+
+ <main class="template-post">
+    <?php
+    if(have_posts()): // Vérifie si il y a des articles à afficher
+        while(have_posts()): // Boucle : tourne tant qu'il y a des articles à afficher
+            the_post();
+    ?>
+    <div>
+        <?php the_content(); // Affiche le contenu de l'article ?>
+    </div>
+    <?php
+    endwhile;
+    endif;
+    ?>
+ </main>
+ <?php
+ get_footer();
