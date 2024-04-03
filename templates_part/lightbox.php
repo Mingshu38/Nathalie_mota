b6215880ba069dd752a21d6f0c82ec84f0e48c0b
Joly Sonia <sonia.joly38@gmail.com>
2024-04-03 15:37:09 +0200

    Creation lightbox

---
+++ templates_part/lightbox.php
@@ -0,0 +1,11 @@
+<div class="lightbox">
+    <div class="lightbox-content">
+        <img src="" alt="" class="lightbox-image">
+        <div class="lightbox-elements">
+            <p class="lightbox-category"></p>
+            <p class="lightbox-reference"></p>
+        </div>
+    </div>
+    <img src="<?php get_the_template_directory_uri() .'/assets/img/left-arrow.png' ?>" alt="flèche image précédente" class="lightbox-previous">
+    <img src="<?php get_the_template_directory_uri() .'/assets/img/right-arrow.png' ?>" alt="Flèche image suivante" class="lightbox-next">
+</div>
--- templates_part/photo-single.php
+++ templates_part/photo-single.php
@@ -2,6 +2,5 @@
 <div class="hover-photo">
     <img src="<?php echo get_template_directory_uri() .'/assets/img/icon_fullscreen.png' ?>" alt="icone fullscreen" class="icon-fullscreen">
     <a href="<?php echo get_permalink() ?>"><img src="<?php echo get_template_directory_uri() .'/assets/img/icon_eye.png' ?>" alt="icone eye" class="icon-eye"></a>
-    <h3><?php echo get_field('reference');?></h3>
     <h3><?php echo get_the_terms(get_the_ID(), 'categorie') [0] ->name ?></h3>
 </div>
