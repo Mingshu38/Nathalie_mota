7826cb9ec2ae175bbea18f28c0fa9ea1f20eeca2
Joly Sonia <sonia.joly38@gmail.com>
2024-05-20 11:14:26 +0200

    work under CSS

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 
 <footer class="footer">
         <nav class="menu-footer">
-		<?php 
-			// Affichage du menu footer déclaré dans functions.php
-			wp_nav_menu(array(
-				'theme_location' => 'footer',
-				'container' =>'ul',  // Pour éviter d'avoir une div autour
-                'menu_class' =>'footer-menu'
-			)); 
-		?>
-		<p class="droits-reserves">Tous droits réservés</p>
+			<?php 
+				// Affichage du menu footer déclaré dans functions.php
+				wp_nav_menu(array(
+					'theme_location' => 'footer',
+					'container' =>'ul',  // Pour éviter d'avoir une div autour
+					'menu_class' =>'footer-menu'
+				)); 
+			?>
+			<p class="droits-reserves">Tous droits réservés</p>
 		</nav>
 
 		<!-- Insertion du template de la modale de contact -->
